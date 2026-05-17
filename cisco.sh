42a381a522480a03179e1552f0e84b1a59758810
Raeed-khan <ethicalhacking5027@gmail.com>
2026-05-18 01:49:57 +0500

    cisco.sh

--- cisco.sh
+++ cisco.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ##   Zphisher 	: 	Automated Phishing Tool
-##   Author 	: 	TAHMID RAYAT 
+##   Author 	: 	Raeed khan 
 ##   Version 	: 	2.3.5
-##   Github 	: 	https://github.com/htr-tech/zphisher
+##   Github 	: 	https://github.com/Raeed-khan/cisco.sh
 
 
 ##                   GNU GENERAL PUBLIC LICENSE
@@ -203,17 +203,13 @@ check_status() {
 ## Banner
 banner() {
 	cat <<- EOF
-		${ORANGE}
-		${ORANGE} ______      _     _     _               
-		${ORANGE}|___  /     | |   (_)   | |              
-		${ORANGE}   / / _ __ | |__  _ ___| |__   ___ _ __ 
-		${ORANGE}  / / | '_ \| '_ \| / __| '_ \ / _ \ '__|
-		${ORANGE} / /__| |_) | | | | \__ \ | | |  __/ |   
-		${ORANGE}/_____| .__/|_| |_|_|___/_| |_|\___|_|   
-		${ORANGE}      | |                                
-		${ORANGE}      |_|                ${RED}Version : ${__version__}
+		${ORANGE}  ____ ___ ____   ____ ___  
+		${ORANGE} / ___|_ _/ ___| / ___/ _ \ 
+		${ORANGE}| |    | |\___ \| |  | | | |
+		${ORANGE}| |___ | | ___) | |__| |_| |
+		${ORANGE} \____|___|____/ \____\___/  ${RED}Version : ${__version__}
 
-		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by htr-tech (tahmid.rayat)${WHITE}
+		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Maintained by cisco (Raeed Khan)${WHITE}
 	EOF
 }
 
@@ -221,12 +217,11 @@ banner() {
 banner_small() {
 	cat <<- EOF
 		${BLUE}
-		${BLUE}  ░▀▀█░█▀█░█░█░▀█▀░█▀▀░█░█░█▀▀░█▀▄
-		${BLUE}  ░▄▀░░█▀▀░█▀█░░█░░▀▀█░█▀█░█▀▀░█▀▄
-		${BLUE}  ░▀▀▀░▀░░░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀░▀${WHITE} ${__version__}
+		${BLUE}  ░█▀▀░▀█▀░█▀▀░█▀▀░█▀█
+		${BLUE}  ░█░░░░█░░▀▀█░█░░░█░█
+		${BLUE}  ░▀▀▀░▀▀▀░▀▀▀░▀▀▀░▀▀▀${WHITE} ${__version__}
 	EOF
 }
-
 ## Dependencies
 dependencies() {
 	echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing required packages..."
